3c701403c1275fdfa8b1092c385b04eccc999554
Paula Cano <paulacanoroman@gmail.com>
2024-07-25 22:47:15 +0200

    feat: counter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import { Counter } from './components/Counter'
 
 function App() {
 
   return (
     <>
       <div>
-       
+       <Counter></Counter>
       </div>
     </>
   )
