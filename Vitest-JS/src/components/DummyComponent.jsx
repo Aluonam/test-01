126af0afe2970123dbdcae01c0eeea37a6237b13
Paula Cano <paulacanoroman@gmail.com>
2024-07-30 10:58:36 +0200

    feat: Ejemplo de Vitest con JS hecho

---
+++ Vitest-JS/src/components/DummyComponent.jsx
@@ -0,0 +1,12 @@
+// src/components/DummyComponent.jsx
+import React from 'react';
+
+const DummyComponent = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
+
+export default DummyComponent;
