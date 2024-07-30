d43b108db738b6597e066154c6a6c084b2341377
Paula Cano <paulacanoroman@gmail.com>
2024-07-30 14:32:33 +0200

    feat: RTL con NEXT y JS

--- pruebas guille/firstTest/vite.config.ts
+++ pruebas guille/firstTest/vite.config.ts
@@ -8,7 +8,7 @@
 
 
 // vitest.config.js
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config.js';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
